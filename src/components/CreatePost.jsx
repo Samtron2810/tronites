7bd596812b9d8f50f93d0ae542b4d50d60b57908
samtron <freegram730@gmail.com>
2026-05-30 17:07:36 +0100

    deployed: added profile pics beside create-post component

--- src/components/CreatePost.jsx
+++ src/components/CreatePost.jsx
@@ -1,29 +1,33 @@
 import { useState } from "react";
-
 import CreatePostModal from "./CreatePostModal";
+import { useAuth } from "../context/AuthContext";
 
-const CreatePost = ({ fetchPosts }) => {
+const CreatePost = () => {
   const [openModal, setOpenModal] = useState(false);
+  const { user } = useAuth();
 
   return (
     <>
       {/* Create Box */}
       <div className="bg-white rounded-2xl shadow-md p-5">
-        <button
-          onClick={() => setOpenModal(true)}
-          className="w-full border border-gray-300 rounded-xl p-4 text-left text-gray-500 hover:border-blue-500 transition cursor-text"
-        >
-          What's happening?
-        </button>
+        <div className="flex items-center gap-3">
+          <img
+            src={user?.profilePic || "https://i.pravatar.cc/"}
+            alt="profile"
+            className="w-12 h-12 rounded-full object-cover border"
+          />
+
+          <button
+            onClick={() => setOpenModal(true)}
+            className="flex-1 border border-gray-300 rounded-xl p-4 text-left text-gray-500 hover:border-blue-500 transition cursor-text"
+          >
+            What's on your mind?
+          </button>
+        </div>
       </div>
 
       {/* Modal */}
-      {openModal && (
-        <CreatePostModal
-          closeModal={() => setOpenModal(false)}
-          fetchPosts={fetchPosts}
-        />
-      )}
+      {openModal && <CreatePostModal closeModal={() => setOpenModal(false)} />}
     </>
   );
 };
